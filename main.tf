b704dfb40863e1e88aed0b849922a3eaae066682
JR <jay.r.reid@gmail.com>
2021-03-28 01:12:56 -0400

    updated files

--- main.tf
+++ main.tf
@@ -6,6 +6,61 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+/*
+ Create an IAM Role for Workflows
+*/
+resource "aws_iam_role" "DataLakeWorkflowRole" {
+  name ="DataLakeWorkflowRole_${var.datalake_abbreviation}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy" "DataLakeWorkflowRolePolicy" {
+  name ="DataLakeWorkflowRolePolicy_${var.datalake_abbreviation}"
+  role = aws_iam_role.DataLakeWorkflowRole.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "LakeFormation",
+        "Effect": "Allow",
+        "Action": [
+          "lakeformation:GetDataAccess",
+          "lakeformation:GetPermissions"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": ["iam:PassRole"],
+        "Resource": [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/DataLakeWorkflowRole_${var.datalake_abbreviation}"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AWSGlueServiceRoleAttach" {
+  role = aws_iam_role.DataLakeWorkflowRole.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
 
 resource "aws_iam_role_policy" "DataAccessPolicyForS3" {
   name ="DataAccessPolicyForS3"
--- variables.tf
+++ variables.tf
@@ -4,6 +4,10 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "datalake_abbreviation" {
+  default = "3740"
+}
+
 variable "s3bucket_name" {
-  default = "lf-datalake-jr" 
+  default = "lf-datalake-${var.datalake_abbreviation}"
 }
